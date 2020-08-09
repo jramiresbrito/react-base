56c35beaeb6d1bab38476660c5f1f58a8521cc3e
Joao Victor Ramires Guimaraes Brito <joaoramires.engsoft@gmail.com>
2020-08-09 19:24:26 -0300

    Chore: Fix with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,19 @@ module.exports = {
     es2020: true,
     node: true,
   },
-  extends: [
-    'plugin:react/recommended', 'airbnb', 'prettier'
-  ],
-  plugins: [
-    'prettier','react',
-  ],
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  plugins: ['prettier', 'react'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 11,
     sourceType: 'module',
-  },  
+  },
   rules: {
     'prettier/prettier': ['error', { singleQuote: true }],
     'no-console': 'off',
     'react/prefer-stateless-function': 'off',
-    'react/jsx-filename-extension': 'off'
+    'react/jsx-filename-extension': 'off',
   },
 };
