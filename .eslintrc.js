1252b76865c0dd898f553abe7bb87af574d02687
Joao Victor Ramires Guimaraes Brito <joaoramires.engsoft@gmail.com>
2020-08-09 17:59:13 -0300

    Chore: Finish the react boilerplate project

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended', 'airbnb', 'prettier'
+  ],
+  plugins: [
+    'prettier','react',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },  
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true }],
+    'no-console': 'off',
+    'react/prefer-stateless-function': 'off',
+    'react/jsx-filename-extension': 'off'
+  },
+};
---
+++ src/components/baseProject/baseProject.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class BaseProject extends Component {
+  render() {
+    return (
+      <div className="padding default-flex-row">
+        <span className="small material-icons">check</span>
+        <span>BASE PROJECT WORKING!!!</span>
+      </div>
+    );
+  }
+}
